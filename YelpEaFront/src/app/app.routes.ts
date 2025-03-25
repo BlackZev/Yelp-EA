b0e0c77efb4b30fc046da5aff4a3847a278f5f6e
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-25 16:29:05 +0100

    Ajout des routes en fonction des routes

--- YelpEaFront/src/app/app.routes.ts
+++ YelpEaFront/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ListRestaurantsComponent } from './list-restaurants/list-restaurants.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'restaurants', component:ListRestaurantsComponent},
+    {path: 'adminpanel', component:AdminPanelComponent},
+    {path: '', redirectTo:'home', pathMatch:'full'},
+    {path: '**', component:NotFoundComponent}
+];
