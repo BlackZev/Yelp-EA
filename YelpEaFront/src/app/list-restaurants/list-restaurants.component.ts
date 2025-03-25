5c2245691b7333c52bb63b64d1b52666ca2fd4c3
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-25 16:26:33 +0100

    Ajout des components ListRestaurants, AdminPanel, NotFound (ng g c <NomDuComponent>)

---
+++ YelpEaFront/src/app/list-restaurants/list-restaurants.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-restaurants',
+  imports: [],
+  templateUrl: './list-restaurants.component.html',
+  styleUrl: './list-restaurants.component.scss'
+})
+export class ListRestaurantsComponent {
+
+}
