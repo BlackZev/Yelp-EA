febdb817a099592303c6c5436dcce791374bba62
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 11:30:12 +0100

    Ajout du component login et de la route pour y acceder

--- YelpEaFront/src/app/app.routes.ts
+++ YelpEaFront/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ListRestaurantsComponent } from './list-restaurants/list-restaurants.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: 'restaurants', component:ListRestaurantsComponent},
     {path: 'adminpanel', component:AdminPanelComponent},
+    {path: 'login', component:LoginComponent},
     {path: '', redirectTo:'restaurants', pathMatch:'full'},
     {path: '**', component:NotFoundComponent}
 ];
