87420c773d896f1f0abdbab8dda1d33a6793caab
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 22:33:21 +0100

    mv de tous les components actuels dans le nouveau dosser components/

--- YelpEaFront/src/app/app.routes.ts
+++ YelpEaFront/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ListRestaurantsComponent } from './list-restaurants/list-restaurants.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
-import { LoginComponent } from './login/login.component';
+import { ListRestaurantsComponent } from '././components/list-restaurants/list-restaurants.component';
+import { NotFoundComponent } from '././components/not-found/not-found.component';
+import { AdminPanelComponent } from '././components/admin-panel/admin-panel.component';
+import { AddRestaurantComponent } from '././components/add-restaurant/add-restaurant.component';
+import { LoginComponent } from '././components/login/login.component';
 
 export const routes: Routes = [
     {path: 'accueil', component:ListRestaurantsComponent},
