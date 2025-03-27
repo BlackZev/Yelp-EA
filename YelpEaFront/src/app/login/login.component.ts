15c9f721b8e04346d5201c26b6fbfbdeab5b99b4
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 20:00:40 +0100

    Modification de card -> form, redirection vers /restaurants si tous les champs sont rempli

--- YelpEaFront/src/app/login/login.component.ts
+++ YelpEaFront/src/app/login/login.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
+  imports: [MatButtonModule, 
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatCheckboxModule, 
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router)
+
+  loginForm = this.formBuilder.group({
+    login: ['', Validators.required],
+    pswd: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/restaurants']);
+    }  }
 }
