84fd2089ebaf7bbd8b987bc68995b2ea978e2a8f
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 21:24:25 +0100

    Redirection home à l'appuie de la toolbar

--- YelpEaFront/src/app/app.component.ts
+++ YelpEaFront/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'YelpEaFront';
+
+  private router = inject(Router);
+  goHome(){
+    this.router.navigate(['/restaurants']);
+  }
 }
