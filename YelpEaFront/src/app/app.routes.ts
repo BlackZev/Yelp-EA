1df18714b2849481696a437639fef2b4f77559b4
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 20:43:20 +0100

    Ajout de AddRestaurantComponent

--- YelpEaFront/src/app/app.routes.ts
+++ YelpEaFront/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ListRestaurantsComponent } from './list-restaurants/list-restaurants.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: 'restaurants', component:ListRestaurantsComponent},
+    {path: 'addRestaurant', component:AddRestaurantComponent},
     {path: 'adminpanel', component:AdminPanelComponent},
     {path: 'login', component:LoginComponent},
     {path: '', redirectTo:'restaurants', pathMatch:'full'},
