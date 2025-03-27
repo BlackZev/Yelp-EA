f27bc555d7aeba4d214c9492973c5438bf393a3b
ABDELKRIM LIMÉRY Zion <blackzev01@gmail.com>
2025-03-27 13:48:18 +0100

    Mise en page du formulaire de connexion

--- YelpEaFront/src/app/login/login.component.ts
+++ YelpEaFront/src/app/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
